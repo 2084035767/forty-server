3038050ea1ba79e360b9bfe21720e9ece0aea631
zs <2084035767@qq.com>
2024-04-22 17:33:18 +0800

    version 1.0.8

--- src/main/java/org/zs/forty/model/vo/LogVO.java
+++ src/main/java/org/zs/forty/model/vo/LogVO.java
@@ -14,19 +14,19 @@ import lombok.NoArgsConstructor;
 @Builder
 @Schema(description = "日志响应")
 public class LogVO implements Serializable {
-    @Serial
-    private static final long serialVersionUID = 1L;
-    
-    @Schema(description = "日志id")
-    private Long logId;
-    @Schema(description = "日志等级")
-    private int logLevel;
-    @Schema(description = "日志名称")
-    private String logName;
-    @Schema(description = "日志信息")
-    private String message;
-    @Schema(description = "用户ID")
-    private Long userId;
-    @Schema(description = "ip地址")
-    private String ipAdd;
+  @Serial
+  private static final long serialVersionUID = 1L;
+  
+  @Schema(description = "日志id")
+  private Long logId;
+  @Schema(description = "日志等级")
+  private int logLevel;
+  @Schema(description = "日志名称")
+  private String logName;
+  @Schema(description = "日志信息")
+  private String message;
+  @Schema(description = "用户ID")
+  private Long userId;
+  @Schema(description = "ip地址")
+  private String ipAdd;
 }
