81fff04d7f91001c19424e7e43aa9280830cdb74
zs <2084035767@qq.com>
2024-04-22 17:35:47 +0800

    version 1.0.8

--- src/main/java/org/zs/forty/service/impl/LogServiceImpl.java
+++ src/main/java/org/zs/forty/service/impl/LogServiceImpl.java
@@ -3,7 +3,6 @@ package org.zs.forty.service.impl;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.zs.forty.common.annotate.MappingIgnore;
 import org.zs.forty.mapper.LogMapper;
 import org.zs.forty.model.dto.LogDTO;
 import org.zs.forty.model.vo.LogVO;
@@ -11,23 +10,22 @@ import org.zs.forty.service.LogService;
 
 @Slf4j
 @Service
-@MappingIgnore
 public class LogServiceImpl implements LogService {
-    @Resource private LogMapper logMapper;
-    
-    @Override public LogVO findByLogId(Long logId) {
-        return logMapper.selectByLogId(logId);
-    }
-    
-    @Override public LogVO addLog(LogDTO logDTO) {
-        return logMapper.selectByLogId(logMapper.add(logDTO));
-    }
-    
-    @Override public Boolean updateLog(LogDTO logDTO) {
-        return logMapper.update(logDTO) > 0;
-    }
-    
-    @Override public Boolean deleteLogByLogId(Long logId) {
-        return logMapper.delete(logId) > 0;
-    }
+  @Resource private LogMapper logMapper;
+  
+  @Override public LogVO findByLogId(Long logId) {
+    return logMapper.selectByLogId(logId);
+  }
+  
+  @Override public LogVO addLog(LogDTO logDTO) {
+    return logMapper.selectByLogId(logMapper.add(logDTO));
+  }
+  
+  @Override public Boolean updateLog(LogDTO logDTO) {
+    return logMapper.update(logDTO) > 0;
+  }
+  
+  @Override public Boolean deleteLogByLogId(Long logId) {
+    return logMapper.delete(logId) > 0;
+  }
 }
