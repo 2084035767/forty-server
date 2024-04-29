5fe274234dca9abfd2d87a11fc9964b9d4b92cc6
zs <2084035767@qq.com>
2024-04-29 11:20:02 +0800

    version 1.0.14

--- src/main/java/org/zs/forty/service/impl/MenuServiceImpl.java
+++ src/main/java/org/zs/forty/service/impl/MenuServiceImpl.java
@@ -22,42 +22,42 @@ import org.zs.forty.service.MenuService;
 @MappingIgnore
 @CacheConfig(cacheNames = "MenuService")
 public class MenuServiceImpl implements MenuService {
-    @Resource private MainMapper mainMapper;
-    @Resource private MenuMapper menuMapper;
-    
-    @Override
-    @Cacheable(key = "#menuId")
-    public MenuVO selectMenuById(Long menuId) {
-        return menuMapper.selectMenuById(menuId);
-    }
-    
-    //    @Override
-    //    public List<MenuVO> selectAllMenus() {
-    //        return null;
-    //    }
-    
-    @Override
-    @CachePut(key = "#menuDTO.menuId")
-    public Boolean update(MenuDTO menuDTO) {
-        return menuMapper.update(menuDTO) > 0;
-    }
-    
-    @Override
-    @Cacheable(key = "#menuDTO.menuId")
-    public MenuVO add(MenuDTO menuDTO) {
-        return mainMapper.menu2VO(menuMapper.selectById(menuMapper.add(menuDTO)));
-    }
-    
-    @Override
-    @CacheEvict(key = "#menuId")
-    public Boolean delete(Long menuId) {
-        return menuMapper.delete(menuId) > 0;
-    }
-    
-    @Override
-    @Cacheable(key = "#pageDTO")
-    public List<MenuVO> findAllMenus(PageDTO pageDTO) {
-        PageHelper.startPage(pageDTO.getPage(), pageDTO.getSize());
-        return menuMapper.selectAllMenus();
-    }
+  @Resource private MainMapper mainMapper;
+  @Resource private MenuMapper menuMapper;
+  
+  @Override
+  @Cacheable(key = "#menuId")
+  public MenuVO selectMenuById(Long menuId) {
+    return menuMapper.selectMenuById(menuId);
+  }
+  
+  //    @Override
+  //    public List<MenuVO> selectAllMenus() {
+  //        return null;
+  //    }
+  
+  @Override
+  @CachePut(key = "#menuDTO.menuId")
+  public Boolean update(MenuDTO menuDTO) {
+    return menuMapper.update(menuDTO) > 0;
+  }
+  
+  @Override
+  @Cacheable(key = "#menuDTO.menuId")
+  public MenuVO add(MenuDTO menuDTO) {
+    return mainMapper.menu2VO(menuMapper.selectById(menuMapper.add(menuDTO)));
+  }
+  
+  @Override
+  @CacheEvict(key = "#menuId")
+  public Boolean delete(Long menuId) {
+    return menuMapper.delete(menuId) > 0;
+  }
+  
+  @Override
+  @Cacheable(key = "#pageDTO")
+  public List<MenuVO> findAllMenus(PageDTO pageDTO) {
+    PageHelper.startPage(pageDTO.getPage(), pageDTO.getSize());
+    return menuMapper.selectAllMenus();
+  }
 }
